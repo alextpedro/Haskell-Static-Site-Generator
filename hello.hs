a367faa822e5ed8412372a4246a71b5ebcbaf9df
Alex Pedro <alextpedro@gmail.com>
2023-10-26 16:45:22 +0100

    Flexible HTML content (functions) Exercises

--- hello.hs
+++ hello.hs
@@ -1,7 +1,11 @@
 main = putStrLn myhtml
-myhtml = html_ (body_ "Hello, world!") 
+myhtml = makeHtml "My page title" "My page content"
 
 wrapHTML content = "<html><body>" <> content <> "</body></html>"
 
 html_ content = "<html>" <> content <> "</html>"
 body_ content = "<body>" <> content <> "</body>"
+head_ content = "<head>" <> content <> "</head>"
+title_ content = "<title>" <> content <> "</title>"
+
+makeHtml title body = html_ (head_ (title_ title) <> body_ body)
