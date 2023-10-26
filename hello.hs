0dfb9ecccfdf0aa85429b29cc718863aee3c9aa7
Alex Pedro <alextpedro@gmail.com>
2023-10-26 18:38:15 +0100

    Adding type signatures Exercises

--- hello.hs
+++ hello.hs
@@ -1,6 +1,10 @@
+main :: IO()
 main = putStrLn myhtml
-myhtml = makeHtml "My page title" "My page content"
 
+myhtml :: String
+myhtml = makeHtml "My page title" (h1_ "Hello, World!" <> p_ "This is a paragraph.")
+
+wrapHTML :: String -> String
 wrapHTML content = "<html><body>" <> content <> "</body></html>"
 
 html_ :: String -> String
@@ -10,10 +14,16 @@ body_ :: String -> String
 body_ = el "body"
 
 head_ :: String -> String
-head_ content = "<head>" <> content <> "</head>"
+head_  = el "head"
 
 title_ :: String -> String
-title_ content = "<title>" <> content <> "</title>"
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
 
 makeHtml :: String -> String -> String
 makeHtml title body = html_ (head_ (title_ title) <> body_ body)
