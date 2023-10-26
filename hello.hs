7e06a328cbbb4c4b0f58d2f79c4a8db90e2ad508
Alex Pedro <alextpedro@gmail.com>
2023-10-26 17:00:00 +0100

    Adding type signatures

--- hello.hs
+++ hello.hs
@@ -3,9 +3,21 @@ myhtml = makeHtml "My page title" "My page content"
 
 wrapHTML content = "<html><body>" <> content <> "</body></html>"
 
-html_ content = "<html>" <> content <> "</html>"
-body_ content = "<body>" <> content <> "</body>"
+html_ :: String -> String
+html_ = el "html"
+
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
 head_ content = "<head>" <> content <> "</head>"
+
+title_ :: String -> String
 title_ content = "<title>" <> content <> "</title>"
 
+makeHtml :: String -> String -> String
 makeHtml title body = html_ (head_ (title_ title) <> body_ body)
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
