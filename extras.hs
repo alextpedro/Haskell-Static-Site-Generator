cd13ac506dc84059c7133059b009bb1dd08548b9
Alex Pedro <alextpedro@gmail.com>
2023-11-29 01:05:19 +0000

    4.2 Pattern matching on linked lists exercises

--- extras.hs
+++ extras.hs
@@ -21,3 +21,47 @@ odd n =
             False
         else
             even (n-1)
+
+isBright :: AnsiColor -> Bool
+isBright ansicolor =
+    case ansicolor of
+        AnsiColor Bright _ -> True
+        AnsiColor Dark _ -> False
+
+ansiToUbuntu :: AnsiColor -> Color
+ansiToUbuntu ansicolor =
+    case ansicolor of
+        AnsiColor brightness color ->
+            case brightness of 
+                Bright -> 
+                    case color of 
+                        Black -> RGB 129 131 131
+                        Red -> RGB 255 0 0
+                        Green -> RGB 0 255 0
+                        Yellow -> RGB 255 255 0
+                        Blue -> RGB 0 0 255
+                        Magenta -> RGB 255 0 255
+                        Cyan -> RGB 0 255 255
+                        White -> RGB 255 255 255
+                Dark ->
+                    case color of
+                        Black -> RGB 0 0 0
+                        Red -> RGB 194 54 33
+                        Green -> RGB 57 181 74
+                        Yellow -> RGB 255 199 6
+                        Blue -> RGB 0 111 184
+                        Magenta -> RGB 118 38 113
+                        Cyan -> RGB 44 181 233
+                        White -> RGB 204 204 204
+
+isEmpty :: [a] -> Bool
+isEmpty list =
+    case listToMaybe list of 
+        Nothing -> True
+        Just _ -> False
+
+isEmpty :: [a] -> Bool
+isEmpty list =
+    case list of 
+        [] -> True
+        _ -> False
