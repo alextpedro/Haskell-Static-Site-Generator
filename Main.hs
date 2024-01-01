07bc8812c1b0c55b41f9130eab4bea8920e24f88
Alex Pedro <alextpedro@gmail.com>
2024-01-01 18:03:04 +0000

    5.2 Working with IO Done

--- Main.hs
+++ Main.hs
@@ -8,38 +8,39 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main =
-    getArgs >>= \args ->
+    args <- getArgs
         case args of
-            [] -> 
-                getContents >>= \content ->
-                    putStrLn (process "Empty title" content)
-            [input, output] ->
-                readFile input >>= \content ->
-                    doesFileExists output >>= \exists ->
+            [] -> do
+               content <- getContents
+               putStrLn (process "Empty title" content)
+            [input, output] -> do
+                content <- readFile input
+                    exists -> doesFileExists output
                         let
                             writeResult = writeFile output (process input content)
                         in
                             if exists
                                 then whenIO confirm writeResult
                                 else writeResult
-            _ -> putStrLn "Usage: runghc Main.hs [--<input-file> <output-file>]"
+            _ -> putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file>]"
 
 process :: Html.Title -> String -> String
 process title = Html.render . convert title . Markup.parse
 
 confirm :: IO Bool
-confirm =
-    putStrLn "Are you sure? (y/n)" *>
-    getLine >>= \answer ->
+confirm = do
+    putStrLn "Are you sure? (y/n)" 
+    answer <- getLine
         case answer of 
             "y" -> pure True
             "n" -> pure False
-            _ -> putStrLn "Invalid response. Use y or n" *>
+            _ -> do 
+                putStrLn "Invalid response. Use y or n" *>
                 confirm
 
 whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = 
-    cond >>= \result ->
+whenIO cond action = do
+    result <- cond
         if result
             then action
             else pure ()
