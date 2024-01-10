a318dbad2d9d59371049e28172fb2565f7468684
Alex Pedro <alextpedro@gmail.com>
2024-01-10 20:09:00 +0000

    5.4 Applicative

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 One day it will be a static blog generator. Today is not that day.
 
 [Read the book](https://learn-haskell.blog).
+
+Concepts new to me: EDSL (Embedded Domain Specific Language)
+
+This book is not great for complete beginners to Haskell with some programming knowledge. It begins easily enough, but ramps up the difficulty pretty fast. In the later chapters especially, it is unclear what is supposed to be an explanation and what is supposed to be implemented.
--- app/Main.hs
+++ app/Main.hs
@@ -4,3 +4,25 @@ import qualified HsBlog
 
 main :: IO ()
 main = HsBlog.main
+
+pInputDir :: Parser FilePath
+pInputDir =
+  strOption
+    ( long "input"
+      <> short 'i'
+      <> metavar "DIRECTORY"
+      <> help "Input directory"
+    )
+
+pOutputDir :: Parser FilePath
+pOutputDir =
+  strOption
+    ( long "output"
+      <> short 'o'
+      <> metavar "DIRECTORY"
+      <> help "Output directory"
+    )
+
+pConvertDir :: Parser Options
+pConvertDir =
+  ConvertDir <$> pInputDir <*> pOutputDir
